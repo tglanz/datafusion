dd19da867b3f2b5b32a44ff4f36cd21b0eb4da1b
Tal Glanzman <tal.glanzman@quantum-art.tech>
2025-04-11 10:23:51 +0300

    create a baseline

---
+++ datafusion-examples/examples/regexp_extract.rs
@@ -0,0 +1,41 @@
+use std::error::Error;
+use std::sync::Arc;
+
+use arrow::array::{ArrayRef, Int32Array, StringArray};
+use arrow::record_batch::RecordBatch;
+
+use datafusion::execution::context::SessionContext;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let ctx = SessionContext::new();
+
+    let a: ArrayRef = Arc::new(StringArray::from(vec![
+        "abcdaxxxd",
+        "asdf",
+        "abab",
+        "cdcd",
+    ]));
+
+    let b: ArrayRef = Arc::new(Int32Array::from(vec![
+        10,
+        20,
+        24,
+        27,
+    ]));
+
+    let batch = RecordBatch::try_from_iter(vec![
+        ("a", a),
+        ("b", b)
+    ])?;
+
+    ctx.register_batch("t", batch)?;
+
+    let df = ctx
+        .sql("SELECT a, regexp_match(a, '(a.*d)', 'i'), b FROM t")
+        .await?;
+
+    df.show().await?;
+
+    Ok(())
+}
